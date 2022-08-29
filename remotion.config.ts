115dc40bec872a5e6b3b763ed9ec8a124cca6751
cheezycode <cheezycode@gmail.com>
2022-08-29 15:54:12 +0530

    ss

--- remotion.config.ts
+++ remotion.config.ts
@@ -3,6 +3,10 @@
 // ! The configuration file does only apply if you render via the CLI !
 
 import {Config} from 'remotion';
+const path = require("path");
+var directoryPath = process.env.PWD;
+
 
 Config.Rendering.setImageFormat('jpeg');
 Config.Output.setOverwriteOutput(true);
+Config.Rendering.setFfmpegExecutable(`${directoryPath}/vendor/ffmpeg/`);
